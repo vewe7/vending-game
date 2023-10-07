d709982447ab82b77faffac4b603f198b5474ac4
vewe7 <naomi.yoon.do@gmail.com>
2023-10-07 13:05:41 -0400

    Add Area2D workaround for collision detection

--- snackstuck.gd
+++ snackstuck.gd
@@ -3,9 +3,15 @@ extends RigidBody2D
 @export var stuckness = 100 # When this reaches 0, the snack should fall
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	print("meow ready")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_body_entered(body):
+	print("meow")
+
+func _on_area_2d_body_entered(body):
+	print(body)

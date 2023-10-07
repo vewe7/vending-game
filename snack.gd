8b941cf5ead66ea6dd0afcf707349d9c05519dbb
vewe7 <naomi.yoon.do@gmail.com>
2023-10-07 15:22:44 -0400

    Rework collision handling to allow indirect collisions

---
+++ snack.gd
@@ -0,0 +1,24 @@
+extends RigidBody2D
+
+signal hit(hit_velocity, hit_mass)
+
+var has_hit = false
+var falling_velocity
+
+@export var stuck_snack_scene: RigidBody2D 
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if (!has_hit):
+		falling_velocity = self.linear_velocity
+
+func _on_body_entered(body):
+	if (!has_hit):
+		has_hit = true
+		stuck_snack_scene.take_hit(falling_velocity, self.mass)
+		
--- snackstuck.gd
+++ snackstuck.gd
@@ -1,14 +1,22 @@
 extends RigidBody2D
 
 @export var stuckness = 100 # When this reaches 0, the snack should fall
+@export var UNSTUCK_CONST = 0.05
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("meow ready")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if (stuckness <= 0):
+		set_freeze_enabled(false)
 	
-func _on_body_entered(body):
-	print(body.linear_velocity.y)
-
+#func _on_body_entered(body):
+#	print(body)
+	
+func take_hit(hit_velocity, hit_mass):
+	stuckness -= abs(hit_velocity.y * hit_mass * UNSTUCK_CONST)
+	print("hit velocity:")
+	print(hit_velocity)
+	print("new stuckness:")
+	print(stuckness)

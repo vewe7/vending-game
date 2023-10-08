c104a667fc6c165c7bd17de3d5e300d94148d9ed
vewe7 <naomi.yoon.do@gmail.com>
2023-10-07 21:15:51 -0400

    Fix snack collisions to ignore non-specific bodies

--- snack.gd
+++ snack.gd
@@ -18,7 +18,7 @@ func _process(delta):
 		falling_velocity = self.linear_velocity
 
 func _on_body_entered(body):
-	if (!has_hit):
+	if (!has_hit && body.scene_file_path == "res://snackstuck.tscn"):
 		has_hit = true
 		stuck_snack_scene.take_hit(falling_velocity, self.mass)
 		

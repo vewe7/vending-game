d7a112b18bd8058c6749dfc67f5a5fe42cd22a27
vewe7 <naomi.yoon.do@gmail.com>
2023-10-08 10:18:56 -0400

    Add logic to switch to end screen upon win

--- snackstuck.gd
+++ snackstuck.gd
@@ -2,6 +2,8 @@ extends RigidBody2D
 
 @export var stuckness = 100 # When this reaches 0, the snack should fall
 @export var UNSTUCK_CONST = 0.07
+var delay_sum = 0
+var win_scene = preload("res://win_screen.tscn").instantiate()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("meow ready")
@@ -10,6 +12,9 @@ func _ready():
 func _process(delta):
 	if (stuckness <= 0):
 		set_freeze_enabled(false)
+		delay_sum += delta
+	if (delay_sum > 1.2):
+		get_tree().change_scene_to_file("res://win_screen.tscn")
 	
 #func _on_body_entered(body):
 #	print(body)

eac5f46bd7c5136b2f024fe5a2747a2c0dc02f1f
vewe7 <naomi.yoon.do@gmail.com>
2023-10-07 23:28:19 -0400

    Add spawner logic

---
+++ spawner.gd
@@ -0,0 +1,21 @@
+extends Node2D
+@export var collision_shape_filename: String
+@export var sprite_filename: String
+var snack_scene = preload("res://snack.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_spawn_item()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _spawn_item():
+	var new_snack = snack_scene.instantiate()
+	new_snack.get_child(0).texture = load("res://sprites/" + sprite_filename)
+	new_snack.get_child(1).shape = load("res://collision_shapes/" + collision_shape_filename)
+	new_snack.stuck_snack_scene = get_parent().get_node("SnackStuck")
+	#new_snack.position = self.position;
+	self.add_child(new_snack)
+	
